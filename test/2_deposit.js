b59107ae1cb2f5fa342e330c24905a57121fd7bd
tspoff@gmail.com <tspoff@gmail.com>
2019-03-29 11:39:22 -0500

    Remove pending deposit storage on-chain

--- test/2_deposit.js
+++ test/2_deposit.js
@@ -27,23 +27,6 @@ contract("RollupNC Deposit", async accounts => {
     assert.equal(logArgs.pubKey, rollupPubKey);
     assert.equal(logArgs.tokenAddr, tokenInstance.address);
     assert.equal(logArgs.amount, amount);
-    
-    // Ensure deposit is correctly added to the pending deposits list
-    result = await rollupInstance.getPendingDeposits.call();
-
-    pendingDeposits = [];
-    pendingDeposits[0] = {
-      sender: result['0'][0],
-      pubKey: result['1'][0].toNumber(),
-      tokenAddr: result['2'][0],
-      amount: result['3'][0].toNumber(),
-    }
-
-    assert.equal(result['0'].length, 1);
-    assert.equal(pendingDeposits[0].sender, depositor);
-    assert.equal(pendingDeposits[0].pubKey, rollupPubKey);
-    assert.equal(pendingDeposits[0].tokenAddr, tokenInstance.address);
-    assert.equal(pendingDeposits[0].amount, amount);
   });
 
   it("should fail without any token allowance", async () => {
@@ -54,41 +37,11 @@ contract("RollupNC Deposit", async accounts => {
     // TODO
   });
 
-  it("should update deposit merkle root successfully", async () => {
+  it("should update valid deposit root successfully", async () => {
     // TODO
   });
 
-  it("should clear pending deposit (only one) when operator collects it for rollup sidechain", async () => {
-    let rollupInstance = await RollupNC.deployed();
-
-    operator = accounts[0];
-    let result = await rollupInstance.rollupPendingDeposits({ from: operator });
-
-    // Should emit event
-    eventName = result.logs[0].event;
-    logArgs = {
-      pendingDepositCount: result.logs[0].args[0].toNumber(),
-    }
-
-    assert.equal(eventName, "DepositRollup");
-    assert.equal(logArgs.pendingDepositCount, 1);
-
-
-    // Should have cleared pending deposit list
-    result = await rollupInstance.getPendingDeposits.call();
-    assert.equal(result['0'].length, 0);
-  });
-
-  it("should clear pending deposits (several) when operator collects them for rollup sidechain", async () => {
+  it("should reject non-operator accounts trying to update deposit root", async () => {
     // TODO
   });
-
-  it("should reject non-operator accounts trying to clear pending deposits", async () => {
-    // TODO
-  });
-
-  it("should track new deposits correctly after previously clearing them", async () => {
-    // TODO
-  });
-
 });
