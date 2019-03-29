fc85d3b589b685ce95b6293ffd23891ac250d1a5
tspoff@gmail.com <tspoff@gmail.com>
2019-03-28 22:23:29 -0500

    add stress test stub

---
+++ test/3_stress.js
@@ -0,0 +1,11 @@
+const RollupNC = artifacts.require("RollupNC");
+const TestToken = artifacts.require("TestToken");
+
+/*
+    Here we want to stress the smart contract with large data sets and verify its behavior and efficeny under those conditions
+*/ 
+
+contract("StressTset", async accounts => {
+  it("should correctly handle large numbers of deposits", async () => {
+  });
+});
