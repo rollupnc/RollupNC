f322655f27716cafb551b303ccf4ab654ca962c7
Fred <fredlacs@gmail.com>
2019-04-08 14:26:56 +0100

    Remove old comments

--- test/mimc.js
+++ test/mimc.js
@@ -24,9 +24,7 @@ contract("MiMC7 Hashing", async accounts => {
         const input = [2];
         const key = 22;
 
-        let result = await mimc.methods.MiMCpe7(input, key).call();
-        // console.log(result);
-        // console.log(result.toString());
+        await mimc.methods.MiMCpe7(input, key).call();
     });
 
     it("Should hash value correctly in js", async () => {
@@ -90,18 +88,4 @@ contract("MiMC7 Hashing", async accounts => {
         assert.equal(multihashG, hashG, "wrong multihash");
         assert.notEqual(multihashG, jsG);
     });
-
-
-
-    // // https://github.com/HarryR/ethsnarks/blob/master/test/TestMiMC.sol
-    // it("should hash value correctly in solidity", async () => {
-    //     var m = [   BigInt(3703141493535563179657531719960160174296085208671919316200479060314459804651).toString(),
-    //                 BigInt(134551314051432487569247388144051420116740427803855572138106146683954151557).toString()  ]
-
-	// 	const k = BigInt(918403109389145570117360101535982733651217667914747213867238065296420114726).toString();
-    //     const expected = BigInt(15683951496311901749339509118960676303290224812129752890706581988986633412003).toString();
-        
-    //     const result = await mimc7.Hash(m, k);
-	// 	assert.equal(result.toString(), expected.toString(), "Unexpected result");
-    // });
 });
