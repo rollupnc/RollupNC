b654dc06a8ab4cb74d77952cdad987833146a417
Fred <fredlacs@gmail.com>
2019-04-27 13:37:02 +0100

    Refactor token deposit test

--- test/2_deposit.js
+++ test/2_deposit.js
@@ -17,30 +17,13 @@ contract("RollupNC Deposit", async accounts => {
     nonce = 0;
 
     // Register Token
-    let result = await registryInstance.registerToken(tokenInstance.address, { from: depositor, value: 1000000000000000000 });
-
-    const tokenIndex = await result.logs[0].args[1].toNumber();
-
-    let result2 = await registryInstance.tokenAddresses(1);
-    console.log("here: result" + (await result2))
-
-
-    result = await registryInstance.getTokenAddressById(0, { from: depositor });
-    // result = "0x1E78E57624c88Cd0466eb5e7e7EAfc410C4a6AEA"
-    console.log("token address by id" + result)
-    
-    let tokenId = await registryInstance.getTokenIdByAddress(tokenInstance.address, { from: depositor });
-    
-    console.log("token id by address: " + await tokenId)
-    console.log(tokenId.toNumber());
+    let result = await registryInstance.registerToken(tokenInstance.address, { from: depositor, value: 10**18 });
+    const tokenIndex = await registryInstance.getTokenIdByAddress(tokenInstance.address, { from: depositor });
     
     // Approve Token Spend
     await tokenInstance.approve(depositInstance.address, balance, { from: depositor });
-    console.log("here3")
-    
     // Deposit Token
     result = await depositInstance.depositTokens(rollupPubKey_x, rollupPubKey_y, tokenIndex, balance, nonce, { from: depositor });
-    console.log("here4")
 
     // Ensure deposit event was emitted correctly
     eventName = result.logs[0].event;
@@ -56,7 +39,7 @@ contract("RollupNC Deposit", async accounts => {
     assert.equal(logArgs.sender, depositor);
     assert.equal(logArgs.pubKey_x, rollupPubKey_x);
     assert.equal(logArgs.pubKey_y, rollupPubKey_y);
-    assert.equal(logArgs.tokenAddr, tokenId);
+    assert.equal(logArgs.tokenAddr, tokenIndex);
     assert.equal(logArgs.amount, balance);
   });
 
