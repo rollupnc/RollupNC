c584fbf21756fabe6ca881ec9665616f856fbe41
Fred <fredlacs@gmail.com>
2019-05-12 15:42:02 +0100

    Refactor variable names and comments

--- contracts/DepositManager.sol
+++ contracts/DepositManager.sol
@@ -2,45 +2,38 @@ pragma solidity >=0.4.21;
 
 import 'openzeppelin-solidity/contracts/token/ERC20/IERC20.sol';
 import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
-// import './MerkleTree.sol';
+import './MerkleTree.sol';
 import './TokenRegistry.sol';
 
-contract DepositManager {      
-    using SafeMath for uint;  
-    // using MerkleTree for MerkleTree.Data;
+contract DepositManager {
+    using SafeMath for uint;
+    using MerkleTree for MerkleTree.Data;
 
     // current merkle root of deposit tree
-    // MerkleTree.Data depositTree;
     uint256 depositTreeRoot;
+    // TODO: should we store the current count, or take it in as a parametre when manipulating the tree?
+    uint256 depositCount;
     TokenRegistry tokenRegistry;
 
-    event PendingDeposit (
-        uint pubKey_x,
-        uint pubKey_y,
-        uint token,
-        uint balance,
-        uint nonce
-        // bytes32 root;
-    );
-
     uint256[] pendingDeposits;
-    address operator;
+    address coordinator;
 
+    event PendingDeposit(uint pubKey_x, uint pubKey_y, uint token, uint balance, uint nonce);
     event DepositAdded(address indexed sender, uint indexed pubKey_x, uint pubKey_y, uint token, uint balance, uint nonce);
     event DepositRootUpdated(uint256 indexed newRoot, uint256 indexed oldRoot);
     event TokenRegistered(address tokenAddr, uint tokenIndex);
 
     constructor(address _tokenRegistryAddress) public {
-        operator = msg.sender;
+        coordinator = msg.sender;
         tokenRegistry = TokenRegistry(_tokenRegistryAddress);
     }
 
-    modifier onlyOperator(){
-        assert(msg.sender == operator);
+    modifier onlyCoordinator(){
+        assert(msg.sender == coordinator);
         _;
     }
 
-    /// @notice Deposit ERC20 tokens into contract and alert Operator. Tokens must be previously approved for this contract by the sender.
+    /// @notice Deposit ERC20 tokens into contract and alert coordinator. Tokens must be previously approved for this contract by the sender.
     /// TODO: We'll want to check signature of rollup pubkey to verify ownership?
     function depositTokens(uint pubKey_x, uint pubKey_y, uint token, uint balance, uint nonce) public {
         address tokenAddr = tokenRegistry.getTokenAddressById(token);
@@ -56,11 +49,11 @@ contract DepositManager {
         emit DepositAdded(msg.sender, pubKey_x, pubKey_y, token, balance, nonce);
     }
 
-    /// @notice Operator publishes deposits root after incorporating new deposits.
+    /// @notice coordinator publishes deposits root after incorporating new deposits.
     /// @dev Will replace params these with the proper inputs and proof.
     /// @param _newDepositTreeRoot New root for deposit merkle tree
     /// TODO Validate Proof
-    function publishDeposits(uint256 _newDepositTreeRoot) external onlyOperator {
+    function publishDeposits(uint256 _newDepositTreeRoot) external onlyCoordinator {
         emit DepositRootUpdated(_newDepositTreeRoot, depositTreeRoot);
         depositTreeRoot = _newDepositTreeRoot;
     }
